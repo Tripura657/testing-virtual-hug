ab2246002bf72dfa74ca187f178709efdfaa5473
Tripura657 <tripurateja4545@gmail.com>
2025-04-04 22:16:38 +0530

    Update hug.py

--- hug.py
+++ hug.py
@@ -1,22 +1,15 @@
 import streamlit as st
 
-# Page config
-st.set_page_config(page_title="Virtual Hug 🤗", layout="wide")
+# Streamlit page setup
+st.set_page_config(page_title="Virtual Hug 🤗", layout="centered")
+st.title("🤗 Here's a virtual hug for you!")
 
-# Center-align using columns
-st.markdown("<h1 style='text-align: center;'>🤗 Here's a virtual hug for you!</h1>", unsafe_allow_html=True)
+# Show a comforting GIF
+st.image("https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif", width=300)
 
-# Full-width layout using columns
-left, center, right = st.columns([1, 3, 1])
-
-with center:
-    st.video("https://media1.tenor.com/m/0bp-O3e9disAAAAd/allen-ma-cravity-allen-ma.gif")
-
-# Heartwarming message centered
+# Display a kind message
 st.markdown("""
-<div style='text-align: center; font-size: 20px; padding-top: 20px;'>
-You're doing great.<br>
-Take a deep breath.<br>
-Everything will be okay. 💙
-</div>
-""", unsafe_allow_html=True)
+You're doing great.  
+Take a deep breath.  
+Everything will be okay. 💙  
+""")
